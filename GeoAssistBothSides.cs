264376effcc5f0feec9dfee7de70f274688ffbfb
Ubergamz <ubergamz@gmail.com>
2022-12-07 15:09:03 -0500

    1.0

--- GeoAssistBothSides.cs
+++ GeoAssistBothSides.cs
@@ -4,6 +4,12 @@ using Mastercam.Database.Types;
 using Mastercam.App.Types;
 using Mastercam.GeometryUtility;
 using Mastercam.Support;
+using System.Collections.Generic;
+using Mastercam.Math;
+using Mastercam.GeometryUtility.Types;
+using Mastercam.Curves;
+using Mastercam.BasicGeometry;
+using System;
 
 namespace _GeoAssistBothSides
 {
@@ -11,28 +17,25 @@ namespace _GeoAssistBothSides
     {
         public Mastercam.App.Types.MCamReturn GeoAssistBothSidesRun(Mastercam.App.Types.MCamReturn notused)
         {
+            var tempList = new List<int>();
             void offsetCutchain(){
                 SelectionManager.UnselectAllGeometry();
                 LevelsManager.RefreshLevelsManager();
                 GraphicsManager.Repaint(true);
 
                 var selectedCutChain = ChainManager.GetMultipleChains("Select Geometry");
-                int mainGeo = 10;
-                int cleanOut = 12;
-                int roughSurf = 138;
-                int finishSurf = 139;
 
                 foreach (var chain in selectedCutChain){
-
                     var mainGeoSide1 = chain.OffsetChain2D(OffsetSideType.Left, .002, OffsetRollCornerType.All, .5, false, .005, false);
                     var mainGeoSide2 = chain.OffsetChain2D(OffsetSideType.Right, .002, OffsetRollCornerType.All, .5, false, .005, false);
                     var mainGeoResult = SearchManager.GetResultGeometry();
                     foreach (var entity in mainGeoResult){
                         entity.Color = 10;
                         entity.Selected = true;
+                        entity.Level = 10;
                         entity.Commit();
                     }
-                    GeometryManipulationManager.MoveSelectedGeometryToLevel(mainGeo, true);
+                    //GeometryManipulationManager.MoveSelectedGeometryToLevel(mainGeo, true);
                     GraphicsManager.ClearColors(new GroupSelectionMask(true));
 
                     var cleanOutSide1 = chain.OffsetChain2D(OffsetSideType.Left, .0025, OffsetRollCornerType.All, .5, false, .005, false);
@@ -41,9 +44,10 @@ namespace _GeoAssistBothSides
                     foreach (var entity in cleanOutResult){
                         entity.Color = 12;
                         entity.Selected = true;
+                        entity.Level = 12;
                         entity.Commit();
                     }
-                    GeometryManipulationManager.MoveSelectedGeometryToLevel(cleanOut, true);
+                    //GeometryManipulationManager.MoveSelectedGeometryToLevel(cleanOut, true);
                     GraphicsManager.ClearColors(new GroupSelectionMask(true));
 
                     var finishSurfSide1 = chain.OffsetChain2D(OffsetSideType.Left, .0005, OffsetRollCornerType.All, .5, false, .005, false);
@@ -52,14 +56,16 @@ namespace _GeoAssistBothSides
                     foreach (var entity in finishSurfResult){
                         entity.Color = 139;
                         entity.Selected = true;
+                        entity.Level = 139;
                         entity.Commit();
                     }
-                    GeometryManipulationManager.MoveSelectedGeometryToLevel(finishSurf, true);
+                    //GeometryManipulationManager.MoveSelectedGeometryToLevel(finishSurf, true);
                     GraphicsManager.ClearColors(new GroupSelectionMask(true));
                 }
             }
             offsetCutchain();
-            GraphicsManager.Repaint(true);
+
+
             return MCamReturn.NoErrors;
         }
     }
